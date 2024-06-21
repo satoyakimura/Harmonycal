dcffe1533a98ec50ba2e2f76e864b20f08d46596
satoyakimura <k.satoya37@gmail.com>
2024-06-21 12:02:32 +0900

    tmp

---
+++ backend/app/api/models/__init__.py
@@ -0,0 +1,2 @@
+from .user_mod import UserModel
+from .schedule_mod import ScheduleModel
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,8 @@
-import os
-from dotenv import load_dotenv
+from pydantic import BaseSettings
 
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+class Settings(BaseSettings):
+    database_url: str
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
