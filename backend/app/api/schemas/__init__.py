0e1f22cfad38b30ec0f71f5741232625398e59d9
satoyakimura <k.satoya37@gmail.com>
2024-06-28 22:47:22 +0900

    restart

--- backend/app/api/schemas/__init__.py
+++
@@ -1,2 +0,0 @@
-from .user_sch import User, UserBase, UserCreate
-from .schedule_sch import Schedule, ScheduleBase, ScheduleCreate
--- db/init/01_init.sql
+++ db/init/01_init.sql
@@ -1,3 +1,12 @@
+-- create role
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'harmonycal') THEN
+        CREATE ROLE harmonycal WITH LOGIN PASSWORD 'password';
+    END IF;
+END $$;
+
+-- create database
 DO $$ 
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'harmonycal') THEN
